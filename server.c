a2650f25f7530a442b3055795a0606c09c21ae84
Mariana <mariana.andrade98@gmail.com>
2021-10-26 21:25:32 +0100

    some info

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mandrade <mandrade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 18:10:27 by mandrade          #+#    #+#             */
-/*   Updated: 2021/10/22 18:24:34 by mandrade         ###   ########.fr       */
+/*   Updated: 2021/10/25 20:01:51 by mandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 int	main(void)
 {
-	struct sigaction	sa_signal;
-	
-	
+	struct sigaction	sa_sig;
+	sigset_t			block_mask;
+
+	sigemptyset(&block_mask);
+	sigaddset(&block_mask, SIGINT);
+	sigaddset(&block_mask, SIGQUIT);
+	sa_sig.sa_handler = 0;
+	sa_sig.sa_flags = SA_SIGINFO;
+	sa_sig.sa_mask = block_mask;
 }
